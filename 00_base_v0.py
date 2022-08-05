9088c0285b2a6b3eb7065f54da20ec56e6dae9ba
amym4649 <107890791+amym4649@users.noreply.github.com>
2022-08-05 15:06:12 +1200

    Base skeleton

---
+++ 00_base_v0.py
@@ -0,0 +1,42 @@
+"""
+Base component for the game 'maths_quiz'
+v0 - skeleton script
+
+author - Amy Macpherson
+CC AM 2022
+"""
+
+
+# Functions
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        # If 'no', show instructions then continue the program
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        # If an invalid answer, request either yes / no
+        else:
+            print("Please enter either yes / no")
+
+
+# Main routine
+
+# Introduction to the game
+print("Welcome to the Maths Quiz!")
+print()
+
+name = input("What is your name? ").capitalize()
+print()
+
+played_before = yes_no("Hi {}! Have you played the game before? ".format(name))
+print()
+
+print("You chose {}.".format(played_before))
