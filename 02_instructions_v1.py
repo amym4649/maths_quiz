35f61cf5d6d782dea0c35ac9455383ea26ac65da
amym4649 <107890791+amym4649@users.noreply.github.com>
2022-08-09 21:29:33 +1200

    Add files via upload
    Instructions component

---
+++ 02_instructions_v1.py
@@ -0,0 +1,8 @@
+print("*** Instructions ***")
+print()
+print("1. Choose the type of basic facts you want to practice\n"
+      "2. When a question is given, type in an integer to answer\n"
+      "3. Press the 'return' key\n"
+      "4. To exit the game, type 'xxx'\n"
+      "5. After the round you will receive your score")
+5
