32d0b016a7cb2f4cfe2c6d585908188812e1d183
amym4649 <107890791+amym4649@users.noreply.github.com>
2022-08-09 21:30:37 +1200

    Add files via upload

--- 00_base_v0.py
+++ 00_base_v0.py
@@ -27,6 +27,16 @@ def yes_no(question):
             print("Please enter either yes / no")
 
 
+def instructions():
+    print("*** Instructions ***")
+    print()
+    print("1. Choose the type of basic facts you want to practice\n"
+          "2. When a question is given, type in an integer to answer\n"
+          "3. Press the 'return' key\n"
+          "4. To exit the game, type 'xxx'\n"
+          "5. After the round you will receive your score")
+
+
 # Main routine
 
 # Introduction to the game
@@ -40,3 +50,8 @@ played_before = yes_no("Hi {}! Have you played the game before? ".format(name))
 print()
 
 print("You chose {}.".format(played_before))
+print()
+
+if played_before == "no":
+    instructions()
+
